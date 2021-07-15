bd35edfc018da9fa7f954518bdc28ba4ab34a10f
Arihant Baid <arihantbaid99@gmail.com>
2021-07-15 16:35:07 +0530

    client

--- Test/client.c
+++ Test/client.c
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <semaphore.h>
 #include <fcntl.h>
+#include "./AES/aes.h"
 
 #define ADDRESS "tcp://broker.hivemq.com:1883"
 #define CLIENTID "PC1"
@@ -18,13 +19,13 @@
 #define TIMEOUT 10000L
 
 int main(int argc, char *argv[])
-{
+{   
     MQTTClient client;
     MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
     MQTTClient_message pubmsg = MQTTClient_message_initializer;
     MQTTClient_deliveryToken token;
     int rc;
-
+    
     MQTTClient_create(&client, ADDRESS, CLIENTID,
                       MQTTCLIENT_PERSISTENCE_NONE, NULL);
     conn_opts.keepAliveInterval = 20;
@@ -37,8 +38,20 @@ int main(int argc, char *argv[])
     }
         //    pubmsg.payload = PAYLOAD;
         //    pubmsg.payloadlen = strlen(PAYLOAD);
-        pubmsg.payload = PAYLOAD;
-        pubmsg.payloadlen = strlen(PAYLOAD);
+
+
+        //AES ENCRYPTION
+        struct AES_ctx Enc;
+        //const uint8_t key[16] = { (uint8_t) 0x2b, (uint8_t) 0x7e, (uint8_t) 0x15, (uint8_t) 0x16, (uint8_t) 0x28, (uint8_t) 0xae, (uint8_t) 0xd2, (uint8_t) 0xa6, (uint8_t) 0xab, (uint8_t) 0xf7, (uint8_t) 0x15, (uint8_t) 0x88, (uint8_t) 0x09, (uint8_t) 0xcf, (uint8_t) 0x4f, (uint8_t) 0x3c };
+        const uint8_t key = 11;
+        uint8_t buf[16] = PAYLOAD;
+        AES_init_ctx(&Enc, &key);
+        AES_CTR_xcrypt_buffer(&Enc, buf, 16);
+
+
+
+        pubmsg.payload  = buf;
+        pubmsg.payloadlen = strlen(buf);
         pubmsg.qos = QOS;
         pubmsg.retained = 0;
 
@@ -47,10 +60,11 @@ int main(int argc, char *argv[])
 
         printf("Message with delivery token %d delivered\n", token);
         sleep(1);
-    }
+    
    pubmsg.payload = END;
    pubmsg.payloadlen = strlen(END);
-   sem_post(ps);
+   
+
 
     MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
     MQTTClient_disconnect(client, 10000);
@@ -58,3 +72,4 @@ int main(int argc, char *argv[])
 
     return rc;
 }
+ 
