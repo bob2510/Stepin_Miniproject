bdbf7b169022b437b750c23f0b35286e0bec38f4
Aadreesh <aadreeshsahu@gmail.com>
2021-07-22 11:52:58 +0530

    updated

--- Build_Env.sh
+++ Build_Env.sh
@@ -1,13 +1,13 @@
 # Building Essential packages
 
-apt-get install build-essential gcc make cmake cmake-gui cmake-curses-gui
-apt-get install fakeroot fakeroot devscripts dh-make lsb-release
-apt-get install libssl-dev
+sudo apt-get install build-essential gcc make cmake cmake-gui cmake-curses-gui
+sudo apt-get install fakeroot fakeroot devscripts dh-make lsb-release
+sudo apt-get install libssl-dev
 
 # Building source and libraries
 
-git clone https://github.com/eclipse/paho.mqtt.c.git
+sudo git clone https://github.com/eclipse/paho.mqtt.c.git
 cd paho.mqtt.c 
-make
+sudo make
 sudo make install
-make html
+sudo make html
